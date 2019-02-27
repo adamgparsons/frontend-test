d9345a6c77e76a305c0a53c642b3c6e0e4db3df4
Adam <adam@adams-air.home>
2019-02-27 08:05:46 +0000

    updated documentation

--- build/scripts/sideNav.js
+++ build/scripts/sideNav.js
@@ -1,1 +1,8 @@
-alert("Hello! I am an alert box!! test");
+const sideNavTag = document.querySelector('.side-nav');
+const sideNavToggleTag = document.querySelector('.side-nav-toggle');
+
+sideNavToggleTag.addEventListener('click', function (e) {
+  e.preventDefault();
+  sideNavToggleTag.classList.toggle('expanded');
+  sideNavTag.classList.toggle('expanded');;
+});
--- src/scripts/sideNav.js
+++ src/scripts/sideNav.js
@@ -1,8 +1,11 @@
+// tags to reference classes
 const sideNavTag = document.querySelector('.side-nav');
 const sideNavToggleTag = document.querySelector('.side-nav-toggle');
 
+//when side nav icon is clicked
 sideNavToggleTag.addEventListener('click', function (e) {
   e.preventDefault();
+  // toggle 'expanded class to side nav toggle and side nav
   sideNavToggleTag.classList.toggle('expanded');
   sideNavTag.classList.toggle('expanded');;
 });
