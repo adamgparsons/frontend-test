3772210f58a023913a392ef33c8f430bbdb2b234
Adam <adam@adams-air.home>
2019-02-27 20:25:53 +0000

    encapsulated sideNav js in a function

--- src/scripts/sideNav.js
+++ src/scripts/sideNav.js
@@ -1,11 +1,13 @@
-// tags to reference classes
-const sideNavTag = document.querySelector('.side-nav');
-const sideNavToggleTag = document.querySelector('.side-nav-toggle');
+const sideNav = (function () {
+  // tags to reference classes
+  const sideNavTag = document.querySelector('.side-nav');
+  const sideNavToggleTag = document.querySelector('.side-nav-toggle');
 
-//when side nav icon is clicked
-sideNavToggleTag.addEventListener('click', function (e) {
-  e.preventDefault();
-  // toggle 'expanded class to side nav toggle and side nav
-  sideNavToggleTag.classList.toggle('expanded');
-  sideNavTag.classList.toggle('expanded');;
-});
+  //when side nav icon is clicked
+  sideNavToggleTag.addEventListener('click', function (e) {
+    e.preventDefault();
+    // toggle 'expanded class to side nav toggle and side nav
+    sideNavToggleTag.classList.toggle('expanded');
+    sideNavTag.classList.toggle('expanded');;
+  });
+})();
